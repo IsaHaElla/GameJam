15a46432235f5059d943a910f1e252215cb4b5ce
IsaHa/Ella <Isabell.Hartwig@games-academy.de>
2023-11-04 13:37:07 +0100

    EnemyPrefabs
    Bei mir hat Unity gesponenn,deswegen habeich de Enemies noch einzelnd als Prefabs reingemacht anstatt als Szene

--- Assets/FlyingEnemy.cs
+++ Assets/FlyingEnemy.cs
@@ -1,13 +1,16 @@
 using System.Collections;
 using System.Collections.Generic;
+using Unity.VisualScripting;
 using UnityEngine;
 
 public class FlyingEnemy : MonoBehaviour
 {
+    public float mMovementSpeed;
+    private GameObject player;
     // Start is called before the first frame update
     void Start()
     {
-        
+        player= GameObject.FindGameObjectWithTag("Player");  
     }
 
     // Update is called once per frame
@@ -15,4 +18,6 @@ public class FlyingEnemy : MonoBehaviour
     {
         
     }
+    private void Chase() 
+    { }
 }
