d6f87a0d8671abed7cb8745054d78375b2212664
IsaHa/Ella <Isabell.Hartwig@games-academy.de>
2023-11-04 20:56:27 +0100

    MainMenu

---
+++ Assets/03_Scripts/MenuControl.cs
@@ -0,0 +1,40 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class MenuControl : MonoBehaviour
+{
+    [Header("Volume Setting")]
+    [SerializeField] private Text volumeTextValue = null;
+    [SerializeField] private Slider volumeSlider = null;
+    [SerializeField] private GameObject confirmationPrompt = null;
+
+    [Header("Levels to Load")]
+    public string _newGameLevel;
+    public void NewGameDialogYes()
+    {
+        SceneManager.LoadScene(_newGameLevel);
+    }
+    public void ExitButton()
+    {
+        Application.Quit();
+    }
+    public void SetVolume(float volume)
+    {
+        AudioListener.volume = volume;
+        volumeTextValue.text  = volume.ToString("0.0");
+    }
+    public void VolumeApply()
+    {
+        PlayerPrefs.SetFloat("mastervolume", AudioListener.volume);
+        StartCoroutine(ConfirmationBox());
+    }
+    public IEnumerator ConfirmationBox()
+    {
+        confirmationPrompt.SetActive(true);
+        yield return new WaitForSeconds(2);
+        confirmationPrompt.SetActive(false);
+    }
+}
