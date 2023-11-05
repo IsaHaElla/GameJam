5282916faa12c972d32b90e859dbbe6f211a4e4e
IsaHa/Ella <Isabell.Hartwig@games-academy.de>
2023-11-05 10:56:20 +0100

    PauseMenuLayout

--- Assets/03_Scripts/MenuControl.cs
+++ Assets/03_Scripts/MenuControl.cs
@@ -24,7 +24,7 @@ public class MenuControl : MonoBehaviour
     public void SetVolume(float volume)
     {
         AudioListener.volume = volume;
-        volumeTextValue.text  = volume.ToString("0.0");
+        volumeTextValue.text  = volume.ToString("0");
     }
     public void VolumeApply()
     {
@@ -34,7 +34,7 @@ public class MenuControl : MonoBehaviour
     public IEnumerator ConfirmationBox()
     {
         confirmationPrompt.SetActive(true);
-        yield return new WaitForSeconds(2);
+        yield return new WaitForSeconds(1);
         confirmationPrompt.SetActive(false);
     }
 }
